797a86687bf14be13c503f48c59ca670ee50e865
Marroquin1 <luis.marroquin@catolica.edu.sv>
2024-02-24 12:01:57 -0600

    final

--- L01_2019MD601-NUMERO_CARNET2/Program.cs
+++ L01_2019MD601-NUMERO_CARNET2/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//INYECCION POR DEPENDENCIA DEL STRING DE CONEXION AL CONTEXTO
+builder.Services.AddDbContext<blogDBContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("equiposDbConnection")
+)
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
